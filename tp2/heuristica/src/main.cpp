de10f4702650b1140cb0972428de2b6c28030234
PCiruzzi <p.ciruzzi@hotmail.com>
2015-06-10 21:17:48 -0300

    Modifico el nombre de las constantes y cambie un vector a un puntero no incializado por un vector comun sin puntero

--- tp2/heuristica/src/main.cpp
+++ tp2/heuristica/src/main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main(int argc, char* argv[]){
 
-	int cantidadConflictos = 3;
-	int cantidadDeSectores = 4;
+	const int CANTIDAD_CONFLICTOS = 3;
+	const int CANTIDAD_SECTORES = 4;
 	int vicepresidencia;
 
 	/* Tengo todos los sectores
@@ -16,11 +16,11 @@ int main(int argc, char* argv[]){
 	vector<Sector* > apoyoSectores;
 
 	// Si el conflicto esta en -1 significa que todavia no se resolvio una postura a tomar
-	vector<int> *conflictos;
+	vector<int> conflictos;
 
 	// Relleno el vector de los conflictos con -1, indica que todavia no tome ninguna postura sobre los conflictos
-	for(int i = 0; i < cantidadConflictos; i++){
-		conflictos->push_back(-1);
+	for(int i = 0; i < CANTIDAD_CONFLICTOS; i++){
+		conflictos.push_back(-1);
 	}
 
 	/*
@@ -111,10 +111,10 @@ int main(int argc, char* argv[]){
 	apoyoSectores.push_back(sindicatos);
 
 	int sectorAResolver = -1;
-	int minimaCantidad = cantidadConflictos;
+	int minimaCantidad = CANTIDAD_CONFLICTOS;
 
 	// Reviso por todos los sectores sin analizar cual es el que requiere solucionar menor cantidad de conflictos para obtener su apoyo
-	for(int i = 0; i < cantidadDeSectores; i++){
+	for(int i = 0; i < CANTIDAD_SECTORES; i++){
 		if( (apoyoSectores[i]->minimaCantidadDeConflictosASolucionar() < minimaCantidad) and ( !apoyoSectores[i]->sectorResuelto()) ){
 			minimaCantidad = apoyoSectores[i]->minimaCantidadDeConflictosASolucionar();
 			sectorAResolver = i;
@@ -122,7 +122,7 @@ int main(int argc, char* argv[]){
 	}
 
 	int cantidadDeConflictosEncontrados = 0;
-	int minCantidadConflictosEncontrados = cantidadConflictos;
+	int minCantidadConflictosEncontrados = CANTIDAD_CONFLICTOS;
 	int opcionAResolver = -1;
 
 
@@ -135,7 +135,7 @@ int main(int argc, char* argv[]){
 	// NO ESTOY SEGURO, LO HICE Y NO LO REVISE
 
 
-	for(int i = 0; i != sectorAResolver and i < cantidadDeSectores; i++){
+	for(int i = 0; i != sectorAResolver and i < CANTIDAD_SECTORES; i++){
 		vector<vector<int> > conflictosSector = apoyoSectores[i]->getPosturasConflictos();
 
 		// Tengo que iterar comparando las distintas opciones para ganar el apoyo de un sector
@@ -143,7 +143,7 @@ int main(int argc, char* argv[]){
 		if( posturasSectorAResolver.size() <= conflictosSector.size() ){
 			for(unsigned int j = 0; j < posturasSectorAResolver.size(); j++){
 				for(unsigned int l = 0; l < conflictosSector.size(); l++){
-					for(unsigned int k = 0; k < cantidadConflictos; k++){
+					for(int k = 0; k < CANTIDAD_CONFLICTOS; k++){
 						if( (posturasSectorAResolver[j][k] != conflictosSector[l][k]) and (posturasSectorAResolver[j][k] >= 0) and (conflictosSector[l][k] >= 0) ){
 							cantidadDeConflictosEncontrados++;
 						}
@@ -158,7 +158,7 @@ int main(int argc, char* argv[]){
 		} else {	// Tengo mas opciones en el sector en el que voy a resolver los conflictos
 			for(unsigned int j = 0; j < conflictosSector.size(); j++){
 				for(unsigned int l = 0; l < posturasSectorAResolver.size(); l++){
-					for(unsigned int k = 0; k < cantidadConflictos; k++){
+					for(int k = 0; k < CANTIDAD_CONFLICTOS; k++){
 						if( (posturasSectorAResolver[l][k] != conflictosSector[j][k]) and (posturasSectorAResolver[l][k] >= 0) and (conflictosSector[j][k] >= 0) ){
 							cantidadDeConflictosEncontrados++;
 						}
